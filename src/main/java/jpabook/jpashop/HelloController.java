5ec0fe6bc456c16fad1a0943dc4ead7d43432094
Blackbutterfly0852 <blackbutterfly0852@gmail.com>
2020-09-10 13:02:27 +0900

    200910 test commit

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){ // model에 data 담은 후 view로 넘김
-        model.addAttribute("data","hello!");
+        model.addAttribute("data","hello!!");
         return "hello"; // view 이름 ex) hello.html
     }
 }
